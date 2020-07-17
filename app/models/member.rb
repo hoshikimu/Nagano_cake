bd5de445d15c56c2ecd2b86ef0dc4711ad0c7b9e
荒見友稀 <aramiyuki@YK-MacBook-Air.local>
2020-07-17 14:51:20 +0900

    [devise member & admin can sign_in]

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,4 +6,25 @@ class Member < ApplicationRecord
   has_many :shipping_addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+# バリデーション
+validates :name_family, presence: true
+validates :name_first, presence: true
+validates :name_family_kana, presence: true
+validates :name_first_kana, presence: true
+validates :email, presence: true
+# numericality => only_integerで整数のみ、
+# ０以上１０未満のみ
+validates :postal_code, presence: true, 
+numericality: {
+  only_integer: true,
+}
+
+validates :phone_number, presence: true, 
+numericality: {
+  only_integer: true,
+}
+
+validates :password, presence: true, length: {minimum: 6}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :members
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+  devise_for :members, controllers: {
+    registrations: 'members/registrations',
+    sessions: 'members/sessions'
+  }
+
+  resources :members do
+    resources :cart_items
+    resources :orders
+    resources :shipping_addresses
+  end
+  
+  resources :items
+  
+  root 'homes#top'
+  get '/about' => 'homes#about'
+
+  namespace :admin do 
+    resources :genres
+    resources :order_items
+    resources :orders
+    resources :members
+    resources :items
+    get '/homes/top' => 'homes#top'
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
