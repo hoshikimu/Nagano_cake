a7aa8b442d1f02e0d4521948414ae222c15bad01
vagrant <vagrant@localhost.localdomain>
2020-07-17 17:50:42 +0000

    [Add] form_for @genreindex

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,12 @@
 class Admin::GenresController < ApplicationController
   def index
-    
+    @genre =Genre.new
   end
 
   def create
-    
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to request.referer
   end
 
   def edit
@@ -13,5 +15,11 @@ class Admin::GenresController < ApplicationController
 
   def update
     
+  end
+
+  private
+
+  def genre_params
+  	params.require(:genre).permit(:name)
   end
 end
