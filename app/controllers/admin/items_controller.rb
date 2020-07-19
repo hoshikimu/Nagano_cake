90ac34cf7af53684c9ddf8bea384a056d7ada139
vagrant <vagrant@localhost.localdomain>
2020-07-19 06:27:10 +0000

    adding item function

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def new
-    @item = Item.new(params_item)
+    @item_new = Item.new
   end
 
   def create
-    
+    @item_new = Item.new(list_params)
+    @item_new.save
+    redirect_to admin_item_path(@item_new)
   end
 
   def index
-    
+    @items = Item.all
   end
 
   def show
@@ -24,5 +26,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   private
-  
+  def list_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :image, :price, :is_sale_status)
+  end
 end
