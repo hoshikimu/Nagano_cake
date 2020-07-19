63cff5decd7bcf268a4a950155ee231d2817ecf2
vagrant <vagrant@localhost.localdomain>
2020-07-19 12:12:30 +0000

    orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-  	params.require(:order).permit(:member_id, :order_status, :created_at)
+  	params.require(:order).permit(:member_id, :order_status, :created_at, :address, :total, :receiver, :postal_code)
   end
 
 end
