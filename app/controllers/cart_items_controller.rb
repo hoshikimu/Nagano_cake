d11575bfef871cab165d8a2953281d818acf48f2
vagrant <vagrant@localhost.localdomain>
2020-07-20 10:29:00 +0000

    Update View Total

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,21 +1,32 @@
 class CartItemsController < ApplicationController
-  def cart_items
-    
+  def index
+    @cart_items = CartItem.all
+    @total = 0
+
   end
 
   def create
-
-  end
-
-  def destroy
-    
-  end
-
-  def destroy_all
-    
+    # @item = Item.find(params[:id])
+    # @cart_item = CartItem.new(cart_item_params)
+    # @cart_item.save
+    # redirect_to action: 'index'
   end
 
   def update
     
   end
+
+  def destroy
+    # ShippingAddress.find_by(id: params[:id], member_id: params[:member_id]).destroy
+    # redirect_to action: 'index'
+  end
+
+  def destroy_all
+    # ShippingAddress.find(params[:member_id]).destroy
+    # redirect_to action: 'index'
+  end
+
+  # private
+  # params.require(:cart_item).permit(:quantity)
+
 end
