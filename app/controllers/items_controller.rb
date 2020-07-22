1622d7bbb44fbba674860118914277f5f5d72c1b
vagrant <vagrant@localhost.localdomain>
2020-07-22 04:47:47 +0000

    add items search function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.where(is_sale_status: true).page(params[:page]).per(12)
     @genres = Genre.all
   end
 
@@ -8,10 +8,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @item_tax_included_price = (@item.price * 1.1).floor
     @genres = Genre.all
+    @member = Member.find(current_member.id)
+    @cart_item = CartItem.new
   end
 
   def genre
     @genre = Genre.find(params[:genre_id])
+    @genre_items = @genre.items.where(is_sale_status: true).page(params[:page]).per(12)
     @genres = Genre.all
   end
+
+  # private
+  # def cart_item_params
+  #   params.require(:cart_item).permit(:quantity, :item_id, :member_id)
+  # end
 end
