0f349e526e31cfe95f7159cb1e4f91e06b24ed21
vagrant <vagrant@localhost.localdomain>
2020-07-22 10:59:15 +0000

    Update Oders View

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     sessions: 'members/sessions',
     passwords: 'members/passwords'
   }
-
+  
   resources :members do
     resources :cart_items
     delete 'cart_items' => 'cart_items#destroy_all', as: 'cart_all_item'
@@ -18,14 +18,14 @@ Rails.application.routes.draw do
     resources :shipping_addresses
   end
   put "/members/:id/hide" => "members#hide", as: 'member_hide'
-
+  
   get 'withdrawal' => 'members#withdrawal'
   
   resources :items
   
   root 'homes#top'
   get '/about' => 'homes#about'
-
+  
   namespace :admin do 
     resources :genres
     resources :order_items
@@ -34,7 +34,10 @@ Rails.application.routes.draw do
     resources :items
     get '/homes/top' => 'homes#top'
   end
-
+  
   get '/:genre_id/items' => 'items#genre', as: 'genre_item'
+  get '/members/:member_id/about' => 'orders#about', as: 'order_about' 
+  get '/members/:member_id/completion' => 'orders#completion', as: 'member_order_completion' 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
