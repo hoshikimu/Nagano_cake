14e9b749d03078d3f084ca684351b6db7fa9e35f
vagrant <vagrant@localhost.localdomain>
2020-07-23 04:54:51 +0000

    [Update] order_items

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OrderItemsController < ApplicationController
   def update
-    
+
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  enum production_status: [ :着手不可, :製作待ち, :制作中, :製作完了 ]
+  enum production_status: [ :着手不可, :製作待ち, :制作中 , :製作完了 ]
 end
