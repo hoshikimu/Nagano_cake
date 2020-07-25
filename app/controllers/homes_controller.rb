4cd7452595d89097d22452275c65d396832e4426
vagrant <vagrant@localhost.localdomain>
2020-07-25 06:45:23 +0000

    fixed

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
     # Item.all ではなく 4つだけにしたい
-    @items = Item.order("RANDOM()").limit(4)
+    @items = Item.where(is_sale_status: true).order("RANDOM()").limit(4)
     @genres = Genre.all
   end
 
