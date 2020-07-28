281ba071368b0e1794690b5a6be3458ce455d7e7
vagrant <vagrant@localhost.localdomain>
2020-07-28 08:07:23 +0000

    [Update]orders index paginate

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   def index
     @member = Member.find(params[:member_id])
     @orders = @member.orders
-    @orders = Order.page(params[:page]).reverse_order
+    @orders = Order.page(params[:page]).reverse_order.per(5)
     @order_items = OrderItem.all
     @total = 0
     @postage = 800
